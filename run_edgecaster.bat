e3492e1312b2b75fabf201bb6f134d95e52ed2b3
Timothy Karpowitz <walrusmcfishsr@gmail.com>
2025-12-01 18:36:50 +0100

    hotfix2

---
+++ run_edgecaster.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Change to the directory this .bat file lives in
+cd /d "%~dp0"
+cd "v1\Edgecaster"
+
+REM If you use the 'py' launcher (usually best on Windows):
+py -m edgecaster.main
+
+REM Keep the window open after the game exits/crashes
+echo.
+echo [Press any key to close this window]
+pause >nul
